33b06d695f18101aca8ee6856f48e5afa9b0c398
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-08-07 11:30:57 -0400

    Color win screen title

--- src/components/WinScreen.jsx
+++ src/components/WinScreen.jsx
@@ -4,7 +4,7 @@ import "../styles/NonGameScreen.css"
 export default function WinScreen({onHome}) {
   return (
     <div className="screen">
-      <h1 className="screen-title win-title">WIN SCREEN: YOU WIN</h1>
+      <h1 className="screen-title win-title">YOU WIN</h1>
       <button className="home-button" onClick={onHome}>HOME</button> 
     </div>
   );
